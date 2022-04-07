34e3387ad2614a2e965c23c71566a3e8a67207fa
Edgardo-R <edgardo291196@gmail.com>
2022-04-06 20:29:45 -0600

    Agregar archivos de proyecto.

---
+++ ProyPRAD/ProyPRAD/Views/PageContactos.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyPRAD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageContactos : ContentPage
+    {
+        public PageContactos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFoto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSalvar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProyPRAD/ProyPRAD/Views/PagePrincipal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyPRAD.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyPRAD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagePrincipal : ContentPage
+    {
+        public PagePrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_places_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btn_contacts_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TBPageContacts());
+        }
+
+        private void btn_AddContacto_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_AddSitio_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
